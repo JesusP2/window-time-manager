5f3bd99a9acdb8732651cba8bed69ab480f8dc1f
JesusP2 <jesus_perez99@hotmail.com>
2023-03-04 16:16:24 -0600

    added tailwind

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,10 @@ socket.addEventListener("error", async (error) => {
   console.error(error);
 });
 </script>
+
 <template>
-  <div>hello</div>
+  <div class="max-w-lg w-full rounded-lg border-neutral-800 h-96 flex flex-col text-neutral-300 mx-auto border mt-4">
+    <div class="h-8 bg-neutral-800 w-full pt-1 pl-4">ACTIVITY</div>
+    <div class="w-full flex-1 bg-neutral-900"></div>
+  </div>
 </template>
