ec65ea9a03e75d1064be23f355a9b6e70a7e134a
JesusP2 <jesus_perez99@hotmail.com>
2023-03-04 16:02:47 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const socket = new WebSocket("ws://localhost:8080");
+socket.addEventListener("message", async ({ data }: MessageEvent<string>) => {
+  console.log(data);
+});
+socket.addEventListener("error", async (error) => {
+  console.error(error);
+});
+</script>
+<template>
+  <div>hello</div>
+</template>
